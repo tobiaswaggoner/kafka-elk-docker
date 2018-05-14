3ae2adbc2ccd8b70d9c06219b634e744f258d765
Tobias Waggoner <tobias.waggoner@gmail.com>
2018-05-14 11:03:10 +0200

    feat: some more zookeeper testing

--- test-client/index.ts
+++ test-client/index.ts
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+// Little zookeeper test routine...
 app.get('/zoo', async (req, res) => {
 
   try {
@@ -22,20 +23,60 @@ app.get('/zoo', async (req, res) => {
       let result: string = "Get received\n";
       console.log("Connecting");
       try {
-        const client = zookeeper.createClient('zoo1:2181', { sessionTimeout: 1000, spinDelay: 10, retries: 0} );
+        const client = zookeeper.createClient('zoo1:2181', { sessionTimeout: 1000, spinDelay: 10, retries: 0 });
         client.on("state", (state) => {
           console.log(state.name + state.name);
-        } )
+        })
         client.once('connected', function () {
           console.log("Connected");
-          result += 'Connected to the server.';
+          result += 'Connected to the server.\n';
+          client.exists("/TestNode", (ex, stat) => {
+            console.log("Exist executed");
+            result += 'Exist executed.\n';
+            if (ex) {
+              client.close();
+              reject(ex);
+              return;
+            }
+            if (stat == null) {
+              console.log("ZNode does not exist.. creating.");
+              client.create("/TestNode", new Buffer('{ "test": "ok"}'), zookeeper.CreateMode.PERSISTENT, (ex, path) => {
+                console.log("Create executed");
+                result += 'Create executed.\n';
+                if (ex) {
+                  client.close();
+                  reject(ex);
+                  return;
+                }
+                console.log("Successfully created Zookeeper node");
+                client.close();
+                resolve(result + '\nOK');
+                return;
+              });
 
-          client.close();
-          resolve(result + "OK");
+            } else {
+              console.log("ZNode does exist.. getting value.");
+              client.getData("/TestNode", (ex, data) => {
+                console.log("Get executed");
+                if (ex) {
+                  client.close();
+                  reject(ex);
+                  return;
+                }
+                console.log("Successfully queried Zookeeper node: " + data.toJSON());
+                result += data.toString() + "\n";
+                resolve(result + '\nOK');
+
+                client.close();
+                return;
+              });
+            }
+          });
+
         });
         client.connect();
         console.log("Connecting in progress");
-      } catch (ex){
+      } catch (ex) {
 
         console.log("Error");
         reject(ex)
@@ -45,9 +86,10 @@ app.get('/zoo', async (req, res) => {
 
     const result = await zooAccess;
     res.send(result);
+
   } catch (ex) {
     res.status(500);
-    res.send(JSON.stringify( ex ));
+    res.send(JSON.stringify(ex));
   }
 
 });
